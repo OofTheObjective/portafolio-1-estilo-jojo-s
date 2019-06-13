acc93a153bafb4f3ca35181d9ef77a6374440659
Leonardo D. Medellin <leo.d.mdev@gmail.com>
2019-06-13 00:35:47 -0500

    cambiosfinales

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Leo D.</title>
+    <link href="https://fonts.googleapis.com/css?family=Anton&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+SC&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="styles/style.css">
     <link rel="icon" type= "image/png" href="img/l.png"/>
     
@@ -14,7 +16,7 @@
 
     <div>
       <figure class ="logo">
-        <img src="" alt="logo.jpeg">
+        <img src="img/logo.png" class="logo" alt="logo.png">
       </figure>
     </div>
         <nav class="menu">
@@ -33,7 +35,7 @@
     </header>
     <section id="hero" class="hero"><!--seccion principal-->
         <h1><spam>Leonardo Daniel</spam>, Desarrollador FullStack y Animador por diversion.</h1>
-        <!--<img src="img/hero.jpg" alt="hero.jpg" width=500px/>  -->  
+        <img src="img/hero.png" alt="hero.jpg">
     </section>
     <section id="portafolio"class="portfolio"><!--seccion portafolio-->
       <h2>Proyectos en Programacion</h2>
@@ -45,7 +47,7 @@
             <p class="project-url"><small><strong>Mirar </strong> www.github.com</small></p>
             <p class="project-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione odio ex et delectus, blanditiis quos exercitationem doloribus reprehenderit numquam hic quaerat enim molestiae architecto at quod, voluptate veritatis incidunt impedit?</p>
         </div>
-        <figure class="project-imageContainer"><img class="project-image" src="img/blog.jpg" alt="blog.jpg" width= 500px></figure>
+        <figure class="project-imageContainer"><img class="project-image" src="img/blog-1.png" alt="blog.jpg" width= 500px></figure>
       </article>
       <article class="project">
         
@@ -56,19 +58,38 @@
             <p class="project-url"><small><strong>Mirar: </strong> https://www.github.com</small></p>
             <p class="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam rem dolorum iure beatae laudantium voluptatem aspernatur optio temporibus non eaque sequi impedit, dolores adipisci numquam inventore ratione vero facere delectus!</p>
         </div>
-        <figure class="project-imageContainer"><img class="project-image" src="img/blog.jpg" alt="red.jpg" width=500px></figure>
+        <figure class="project-imageContainer"><img class="project-image" src="img/blog-1.png" alt="red.jpg" width=500px></figure>
       </article>
-    </section>
+    </section><!--end portafolio-->
     <section id="contacto" class="contact"><!--seccion de contacto-->
+    <div class="contact-form">
       <form action="suscription.php" class="form-email">
-          <h3><strong>Contactame</strong></h3>
-          <input type="text" placeholder ="correo@contacto.com"> 
-          <h4>Asunto</h4>
-          <textarea name="asunto" id="Asunto" cols="40" rows="5"></textarea>
-          <input type="submit" value="Enviar">    
-      </form>
-    </section>
-    <footer><p><small><strong>Todos los derechos reservados &copy Leonardo D. Gtz Medellin.</strong></small></p></footer>
+            <h3><strong>Contactame</strong></h3>
+            <input type="email" placeholder ="Correo@contacto.com" class="form email"> 
+            <h4>Asunto</h4>
+            <textarea name="asunto" class="form Asunto" cols="40" rows="5"></textarea>
+            <input type="submit" value="Enviar" class="enviar">    
+        </form>
+    </div> 
+    <div class="contact-estilo">
+      <div class="contact-image">
+          <img src="img/contact-image-2.png" alt="contacto.jpg">
+      </div> 
+      
+    </div>
+  </section><!--end contact-->
+  <footer class="footer">
+    <div>
+      <p><small><strong>Todos los derechos reservados &copy Leonardo D. Gtz Medellin.</strong></small></p>
+    </div>
+    <div class="social">
+      <a href="https://twitter.com" class="social-link twitter"></a>
+      <a href="https://github.com" class="social-link github"></a>
+      <a href="https://instagram.com" class="social-link instagram"></a>
+      <a href="https://facebook.com" class="social-link facebook"></a>
+    </div>
+    </footer>
+    
     
 </body>
 </html>
